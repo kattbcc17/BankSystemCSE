a2ebff88cd38a89449b78f50e863e595fc0873ae
kattbcc17 <kattbcc17@byui.edu>
2024-07-06 16:05:46 -0500

    Initial Commit

---
+++ BankSystem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BankSystem
+//
+//  Created by kattia contreras on 7/6/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
